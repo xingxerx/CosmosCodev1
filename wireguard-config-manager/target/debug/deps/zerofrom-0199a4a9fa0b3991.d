1236a66b64313bfbad57dc7097f2e83ffaaa0520
xingxerx <jemonemcc2@gmail.com>
2025-05-18 04:51:38 -0400

    v44

---
+++ wireguard-config-manager/target/debug/deps/zerofrom-0199a4a9fa0b3991.d
@@ -0,0 +1,9 @@
+/mnt/f/CosmosCode/wireguard-config-manager/target/debug/deps/libzerofrom-0199a4a9fa0b3991.rmeta: /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/lib.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/macro_impls.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/zero_from.rs
+
+/mnt/f/CosmosCode/wireguard-config-manager/target/debug/deps/libzerofrom-0199a4a9fa0b3991.rlib: /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/lib.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/macro_impls.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/zero_from.rs
+
+/mnt/f/CosmosCode/wireguard-config-manager/target/debug/deps/zerofrom-0199a4a9fa0b3991.d: /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/lib.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/macro_impls.rs /home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/zero_from.rs
+
+/home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/lib.rs:
+/home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/macro_impls.rs:
+/home/xingxerx/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-0.1.6/src/zero_from.rs:
