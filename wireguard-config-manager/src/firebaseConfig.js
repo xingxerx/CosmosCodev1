a9ae8ab5a8dc136e084ead1d590852713fa19fee
xingxerx <jemonemcc2@gmail.com>
2025-05-18 09:49:11 -0400

    v66

---
+++ wireguard-config-manager/src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
