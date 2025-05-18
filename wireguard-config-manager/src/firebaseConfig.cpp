64dc82f7b6082c8733fd221d0090dacd35f520b9
xingxerx <jemonemcc2@gmail.com>
2025-05-18 05:23:48 -0400

    v50

---
+++ wireguard-config-manager/src/firebaseConfig.cpp
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is also included
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID" // (optional)
+};
+
+firebase.initializeApp(firebaseConfig);
